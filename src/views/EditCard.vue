4d307ca5217e96666d3cceda6e7f39a130ed5ebc
Adam Sapraliev <sapraliev@gmail.com>
2020-09-07 15:49:46 +0700

    Use map mutations

--- src/views/EditCard.vue
+++ src/views/EditCard.vue
@@ -7,16 +7,19 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
 import CardForm from '../components/CardForm';
 import formMixin from '../mixins/formMixin';
+import { UPDATE_CARD } from '../store/mutation-types';
 
 export default {
   name: 'EditCard',
   mixins: [formMixin],
   components: { CardForm },
   computed: {
-    ...mapGetters(['getCard']),
+    ...mapGetters({
+      getCard: 'getCard'
+    }),
     card() {
       return this.getCard(this.$route.params.uuid);
     }
@@ -26,10 +29,13 @@ export default {
     this.form = { title, description };
   },
   methods: {
+    ...mapMutations({
+      updateCard: UPDATE_CARD
+    }),
     onSubmit() {
       const card = this.card;
 
-      this.$store.commit('UPDATE_CARD', {
+      this.updateCard({
         uuid: card.uuid,
         title: this.form.title,
         description: this.form.description
